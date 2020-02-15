23340f258f9f08d4948dbedd1f0e910e8f406baf
Emily Speckhals <emily.speckhals@gmail.com>
2020-02-15 12:23:57 -0500

    move TargetAndIntake to Intake folder

--- src/main/java/frc/robot/commands/Intake/TargetAndIntake.java
+++ src/main/java/frc/robot/commands/Intake/TargetAndIntake.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands.Auto;
+package frc.robot.commands.Intake;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.Intake.LimeLightIntake;
